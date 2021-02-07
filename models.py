d5e36e517de1719a349056920ccc4cb9f0c0f569
Jurgis <jurgisp@gmail.com>
2021-02-07 15:00:11 +0200

    VAE

--- models.py
+++ models.py
@@ -2,32 +2,47 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
+from modules import *
 
-def flatten(x):
-    return torch.reshape(x, (-1,) + x.shape[2:]), x.size(0)
 
-def unflatten(x, n):
-    return torch.reshape(x, (n, -1) + x.shape[1:])
-
-class Autoencoder(nn.Module):
+class VAE(nn.Module):
 
     def __init__(self, encoder, decoder):
         super().__init__()
         self._encoder = encoder
         self._decoder = decoder
+        self._posterior = Posterior(encoder.out_dim, encoder.out_dim, 256, decoder.in_dim)
 
-    def forward(self, obs, action):
-        # Input:
-        #   obs: tensor(N, B, C, H, W)
-        # Output:
-        # 
+    def forward(self,
+                obs,  # tensor(N, B, C, H, W)
+                action):
+
         obs, n = flatten(obs)
-
         embed = self._encoder(obs)
-        pred_obs = self._decoder(embed)
 
-        pred_obs = unflatten(pred_obs, n)
-        return pred_obs
+        deter = torch.zeros_like(embed)
+        post_mean, post_std = self._posterior(deter, embed)
+        post_sample = diag_normal(post_mean, post_std).rsample()
+        obs_reconstr = self._decoder(post_sample)
 
-    def loss(self, output, target):
-        return self._decoder.loss(output, target)
+        return (
+            unflatten(obs_reconstr, n),  # tensor(N, B, C, H, W)
+            unflatten(post_mean, n),     # tensor(N, B, S)
+            unflatten(post_std, n),      # tensor(N, B, S)
+        )
+
+    def loss(self,
+             obs_reconstr, post_mean, post_std,  # forward() output
+             obs_target,                         # tensor(N, B, C, H, W)
+             ):
+        prior_mean = torch.zeros_like(post_mean)
+        prior_std = torch.ones_like(post_std)
+        loss_kl = D.kl.kl_divergence(diag_normal(post_mean, post_std), diag_normal(prior_mean, prior_std))
+        loss_obs = self._decoder.loss(obs_reconstr, obs_target)
+        assert loss_kl.shape == loss_obs.shape  # Should be (N, B)
+
+        loss_kl = loss_kl.mean()  # Mean over (N, B)
+        loss_obs = loss_obs.mean()
+        loss = loss_kl + loss_obs
+        metrics = dict(loss_kl=loss_kl.detach(), loss_obs=loss_obs.detach())
+        return loss, metrics
--- train_offline.py
+++ train_offline.py
@@ -1,5 +1,6 @@
 import argparse
 import inspect
+import pathlib
 from collections import defaultdict
 import numpy as np
 import torch
@@ -9,15 +10,18 @@ import mlflow
 
 from data import OfflineData
 from preprocessing import MinigridPreprocess
-from models import Autoencoder
+from models import VAE
 from modules import MinigridEncoder, MinigridDecoder
 
 
 def main(run_name='adhoc',
          input_dir='./data',
-         batch_length=50,
-         batch_size=50,
-         log_interval=40):
+         batch_length=10,
+         batch_size=25,
+         log_interval=40,
+         save_path='./log/model.pt',
+         save_interval=400,
+         ):
 
     mlflow.start_run(run_name=run_name)
 
@@ -25,7 +29,7 @@ def main(run_name='adhoc',
 
     preprocess = MinigridPreprocess()
 
-    model = Autoencoder(
+    model = VAE(
         encoder=MinigridEncoder(),
         decoder=MinigridDecoder())
     print(f'Model: {sum(p.numel() for p in model.parameters() if p.requires_grad)} parameters')
@@ -41,7 +45,7 @@ def main(run_name='adhoc',
 
         image = preprocess(batch)
         output = model(image, None)
-        loss = model.loss(output, image).mean()
+        loss, loss_metrics = model.loss(*output, image)
 
         # Grad step
 
@@ -52,16 +56,31 @@ def main(run_name='adhoc',
 
         # Metrics
 
-        metrics['loss'].append(loss.item())
         frames += image.size(0) * image.size(1)
         batches += 1
+        metrics['loss'].append(loss.item())
+        for k, v in loss_metrics.items():
+            metrics[k].append(v.item())
+
+        # Log
+
         if batches % log_interval == 0:
             metrics = {k: np.mean(v) for k, v in metrics.items()}
             metrics['_step'] = frames
+            metrics['_loss'] = metrics['loss']
+            metrics['batch'] = batches
+
             print(f"[{frames:07}]"
                   f"  loss: {metrics['loss']:.3f}")
             mlflow.log_metrics(metrics, step=frames)
             metrics = defaultdict(list)
+
+        # Save
+
+        if batches % save_interval == 0:
+            pathlib.Path(save_path).parent.mkdir(parents=True, exist_ok=True)
+            torch.save(model.state_dict(), save_path)
+            print(f'Saved to {save_path}')
 
 
 if __name__ == '__main__':
