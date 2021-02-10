abd24c14f9d92f78912d4f793f66ba8efac96706
Jurgis <jurgisp@gmail.com>
2021-02-10 17:25:41 +0200

    Random skip first file; reset field

--- data.py
+++ data.py
@@ -11,10 +11,9 @@ class OfflineData:
         print(f'Offline data: {len(self._files)} episodes in {str(input_dir)}')
 
     def iterate(self, batch_length, batch_size):
-        # TODO: how many times should we iterate?
-        # TODO: should offset single iterators by random amount, so episode starts are not synchronized
-
         # Parallel iteration over (batch_size) iterators
+        # Iterates forever
+
         iters = [self._iter_single(batch_length) for _ in range(batch_size)]
         for batches in zip(*iters):
             batch = {}
@@ -23,19 +22,30 @@ class OfflineData:
             yield batch
 
     def _iter_single(self, batch_length):
+        # Iterates "single thread" forever
         # TODO: join files so we don't miss the last step indicating done
 
-        for file in self._shuffle_files():
-            for batch in self._iter_file(file, batch_length):
-                yield batch
+        is_first = True
+        while True:
+            for file in self._shuffle_files():
+                for batch in self._iter_file(file, batch_length, skip_random=is_first):
+                    yield batch
+                is_first = False
 
-    def _iter_file(self, file, batch_length):
+    def _iter_file(self, file, batch_length, skip_random=False):
         with file.open('rb') as f:
             fdata = np.load(f)
             data = {key: fdata[key] for key in fdata}
 
         n = data['image'].shape[0]
-        for i in range(0, n - batch_length + 1, batch_length):
+        data['reset'] = np.zeros(n, bool)
+        data['reset'][0] = True  # Indicate episode start
+
+        i_start = 0
+        if skip_random:
+            i_start = np.random.randint(n - batch_length)
+
+        for i in range(i_start, n - batch_length + 1, batch_length):
             # TODO: should return last shorter batch
             j = i + batch_length
             batch = {key: data[key][i:j] for key in data}
