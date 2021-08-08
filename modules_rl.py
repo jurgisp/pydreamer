70f7414a04a07b182beb83d5ad77923b47c8fda0
Jurgis <jurgisp@gmail.com>
2021-08-08 15:56:44 +0300

    Critic target updates

--- modules_rl.py
+++ modules_rl.py
@@ -10,13 +10,17 @@ from modules_common import *
 
 class ActorCritic(nn.Module):
 
-    def __init__(self, in_dim, out_actions, hidden_dim=400, hidden_layers=4):
+    def __init__(self, in_dim, out_actions, hidden_dim=400, hidden_layers=4, gamma=0.99, temperature=1e-3, target_update_interval=100):
         super().__init__()
         self.in_dim = in_dim
         self.out_actions = out_actions
+        self._gamma = gamma
+        self._temperature = temperature
+        self._target_update_interval = target_update_interval
         self._actor = MLP(in_dim, out_actions, hidden_dim, hidden_layers)
         self._critic = MLP(in_dim, 1, hidden_dim, hidden_layers)
         self._critic_target = MLP(in_dim, 1, hidden_dim, hidden_layers)
+        self._train_steps = 0
 
     def forward_act(self, features: Tensor) -> D.Categorical:
         y = self._actor(features)
@@ -30,6 +34,10 @@ class ActorCritic(nn.Module):
         return a
 
     def train(self, features: TensorJMF, rewards: TensorJM2, terminals: TensorJM2, actions: TensorHMA) -> Tuple[Tensor, Dict[str, Tensor]]:
+        if self._train_steps % self._target_update_interval == 0:
+            self.update_critic_target()
+        self._train_steps += 1
+
         reward: TensorHM = F.softmax(rewards[1:], -1).select(-1, 1)  # select(-1,1) => probability of getting reward=1
         terminal: TensorHM = F.softmax(terminals[1:], -1).select(-1, 1)  # select(-1,) => probability of terminal state
         policy = self.forward_act(features[:-1])
@@ -37,7 +45,7 @@ class ActorCritic(nn.Module):
         value_baseline: TensorHM = self._critic_target.forward(features[:-1]).detach()
         value_target: TensorHM = self._critic_target.forward(features[1:]).detach()
 
-        target: TensorHM = reward + 0.99 * value_target * (1.0 - terminal)
+        target: TensorHM = reward + self._gamma * value_target * (1.0 - terminal)
         assert not target.requires_grad
         loss_value = torch.square(value - target)
         loss_value = loss_value.mean()
@@ -48,10 +56,10 @@ class ActorCritic(nn.Module):
         loss_critic = loss_critic.mean()
 
         policy_entropy = policy.entropy().mean()
-        loss_entropy = - 1e-3 * policy_entropy
+        loss_entropy = - self._temperature * policy_entropy
 
-        # TODO: update target networks
         # TODO: need weights, even for H=1, weight=terminals[0]
+        # TODO: lambda-GAE for H>1
 
         with torch.no_grad():
             metrics = dict(loss_ac_value=loss_value.detach(),
@@ -62,3 +70,6 @@ class ActorCritic(nn.Module):
 
         loss = loss_value + loss_critic + loss_entropy
         return loss, metrics
+
+    def update_critic_target(self):
+        self._critic_target.load_state_dict(self._critic.state_dict())  # type: ignore
