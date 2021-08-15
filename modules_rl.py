195463751b8a638ca1a6632bc16be00758393c02
Jurgis <jurgisp@gmail.com>
2021-08-15 14:31:42 +0300

    reality_weight from terminals

--- modules_rl.py
+++ modules_rl.py
@@ -33,20 +33,21 @@ class ActorCritic(nn.Module):
             self.update_critic_target()
         self._train_steps += 1
 
-        reward: TensorHM = rewards.mean[1:]
-        terminal: TensorHM = terminals.mean[1:]
+        reward1: TensorHM = rewards.mean[1:]
+        terminal0: TensorHM = terminals.mean[:-1]
+        terminal1: TensorHM = terminals.mean[1:]
         policy = self.forward_act(features[:-1])
         value0: TensorHM = self._critic.forward(features[:-1])
         value0t: TensorHM = self._critic_target.forward(features[:-1]).detach()
         value1t: TensorHM = self._critic_target.forward(features[1:]).detach()
-        advantage = - value0t + reward + self._gamma * (1.0 - terminal) * value1t
+        advantage = - value0t + reward1 + self._gamma * (1.0 - terminal1) * value1t
         assert not advantage.requires_grad
 
         # GAE from https://arxiv.org/abs/1506.02438 eq (16)
         #   advantage_gae[t] = advantage[t] + (gamma lambda) advantage[t+1] + (gamma lambda)^2 advantage[t+2] + ...
         advantage_gae = []
         agae = None
-        for adv, term in zip(reversed(advantage.unbind()), reversed(terminal.unbind())):
+        for adv, term in zip(reversed(advantage.unbind()), reversed(terminal1.unbind())):
             if agae is None:
                 agae = adv
             else:
@@ -67,26 +68,29 @@ class ActorCritic(nn.Module):
         # where
         #   g[t] = gamma * (1 - terminal[t])
         # is the adjusted discount
-        
+
         # if self._lambda == 1:
         #     value_target_mc = []
         #     v = value1t[-1]
-        #     for rew, term in zip(reversed(reward.unbind()), reversed(terminal.unbind())):
+        #     for rew, term in zip(reversed(reward.unbind()), reversed(terminal1.unbind())):
         #         v = rew + self._gamma * (1.0 - term) * v
         #         value_target_mc.append(v)
         #     value_target_mc.reverse()
         #     value_target_mc = torch.stack(value_target_mc)
         #     assert torch.allclose(value_target, value_target_mc)
 
-        # TODO: weigh losses, even for H=1 weight=terminals[0]
+        # When calculating losses, should ignore terminal states, or anything after, so:
+        #   reality_weight[i] = (1-terminal[0]) (1-terminal[1]) ... (1-terminal[i])
+        # Note this takes care of the case when initial state features[0] is terminal - it will get weighted by (1-terminals[0]).
+        reality_weight = (1 - terminal0).log().cumsum(dim=0).exp()
 
         loss_value = torch.square(value_target - value0)
-        loss_value = loss_value.mean()
+        loss_value = (loss_value * reality_weight).mean()
 
         loss_policy = - (policy.logits * actions).sum(-1) * advantage
-        loss_policy = loss_policy.mean()
+        loss_policy = (loss_policy * reality_weight).mean()
 
-        policy_entropy = policy.entropy().mean()
+        policy_entropy = (policy.entropy() * reality_weight).mean()
         loss_entropy = - self._temperature * policy_entropy
 
         with torch.no_grad():
