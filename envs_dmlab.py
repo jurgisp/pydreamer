a06b880d39f4696e6b0a782eab7ca091c12cb529
Jurgis <jurgisp@gmail.com>
2021-09-22 21:27:17 +0300

    All DMLab30 envs

--- envs_dmlab.py
+++ envs_dmlab.py
@@ -51,6 +51,41 @@ ACTION_SET = {  # R2D2 action set
     14: (0, 0, 0, 0, 1, 0, 0),    # Fire
 }
 
+ALL_GAMES = frozenset([
+    'rooms_collect_good_objects_train',  # rooms_collect_good_objects
+    'rooms_collect_good_objects_test',  # rooms_collect_good_objects
+    'rooms_exploit_deferred_effects_train',  # rooms_exploit_deferred_effects
+    'rooms_exploit_deferred_effects_test',  # rooms_exploit_deferred_effects
+    'rooms_select_nonmatching_object',
+    'rooms_watermaze',
+    'rooms_keys_doors_puzzle',
+    'language_select_described_object',
+    'language_select_located_object',
+    'language_execute_random_task',
+    'language_answer_quantitative_question',
+    'lasertag_one_opponent_small',
+    'lasertag_three_opponents_small',
+    'lasertag_one_opponent_large',
+    'lasertag_three_opponents_large',
+    'natlab_fixed_large_map',
+    'natlab_varying_map_regrowth',
+    'natlab_varying_map_randomized',
+    'skymaze_irreversible_path_hard',
+    'skymaze_irreversible_path_varied',
+    'psychlab_arbitrary_visuomotor_mapping',
+    'psychlab_continuous_recognition',
+    'psychlab_sequential_comparison',
+    'psychlab_visual_search',
+    'explore_object_locations_small',
+    'explore_object_locations_large',
+    'explore_obstructed_goals_small',
+    'explore_obstructed_goals_large',
+    'explore_goal_locations_small',
+    'explore_goal_locations_large',
+    'explore_object_rewards_few',
+    'explore_object_rewards_many',
+])
+
 
 class DmLab(gym.Env):
     """DeepMind Lab wrapper."""
@@ -67,7 +102,9 @@ class DmLab(gym.Env):
                 datasetPath='',  # dataset needed for psychlab_*, see https://github.com/deepmind/lab/tree/master/data/brady_konkle_oliva2008
                 maxAltCameraHeight='1',
                 maxAltCameraWidth='1',
-                hasAltCameras='false'),
+                hasAltCameras='false',
+                allowHoldOutLevels='true',
+                ),
         )
         self._action_set = action_set
         self.action_space = gym.spaces.Discrete(len(self._action_set))  # type: ignore
