b1824fd7c0c5744c28651ec6daf67f7a3182d22a
Jurgis <jurgisp@gmail.com>
2022-05-12 20:12:53 +0300

    Restart wrapper for embodied Minecraft

--- pydreamer/envs/embodied.py
+++ pydreamer/envs/embodied.py
@@ -1,3 +1,4 @@
+import functools
 from typing import List
 
 import gym
@@ -5,6 +6,7 @@ import gym.spaces
 from pydreamer.tools import print_once
 
 import embodied
+from embodied.envs import load_single_env
 
 
 class EmbodiedEnv(gym.Env):
@@ -15,8 +17,13 @@ class EmbodiedEnv(gym.Env):
                  action_repeat=1,
                  time_limit=0,
                  obs_keys=['image', 'inventory', 'equipped'],  # TODO: this default is for Minecraft
+                 restart=True,  # restart needed for Minecraft
                  ):
-        self._env = embodied.envs.load_single_env(task, repeat=action_repeat, length=time_limit)
+        ctor = functools.partial(load_single_env, task, repeat=action_repeat, length=time_limit)
+        if restart:
+            self._env = embodied.wrappers.RestartOnException(ctor)
+        else:
+            self._env = ctor()
         self.obs_keys = obs_keys
         self.action_space = EmbodiedEnv.convert_act_space(self._env.act_space)
         self.observation_space = EmbodiedEnv.convert_obs_space(self._env.obs_space, obs_keys)
