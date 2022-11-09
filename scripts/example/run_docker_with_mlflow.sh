6bb44ecc16810ec4d452bea257b6b01f55d9e533
Jurgis <jurgisp@gmail.com>
2022-11-09 15:23:14 +0200

    Cleanup scripts

--- scripts/example/run_docker_with_mlflow.sh
+++
@@ -1,8 +0,0 @@
-docker build . -f Dockerfile -t pydreamer
-
-mlflow server &
-
-docker run -it \
-    -e MLFLOW_TRACKING_URI="http://docker.for.mac.host.internal:5000" --net=host \
-    pydreamer \
-    sh scripts/xvfb_run.sh python3 launch.py --configs defaults atari debug
